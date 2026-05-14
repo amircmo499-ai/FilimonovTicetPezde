1d7463fe849b029f0446c8475bf78f94d04df679
Dalina <zalupaevvyelfi@gmail.com>
2026-05-14 15:30:19 +0600

    Add links and add folder

---
+++ ticket28/ticket28.cs
@@ -0,0 +1,32 @@
+using System;
+
+
+//билет 28
+namespace Ticket
+{
+    
+    class Program
+    {
+        static async Task Main(string[] args)
+        { 
+            //получаем какойто обьект
+            object  data = "Dalina";
+
+            //переводим безопастно его в строку
+            string text = data as string;
+
+            //проверяем далась нам строка или нет
+            if(text == null)
+                Console.WriteLine("это не текст");
+            else
+                Console.WriteLine(text.Length());
+            
+                
+             
+        }
+       
+        
+    }
+
+ 
+}
