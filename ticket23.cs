3b285a897e5a78badebb14e31571d6309122d9e3
Dalina <zalupaevvyelfi@gmail.com>
2026-05-15 23:30:45 +0600

    add information and fix ticket3.md ticket23.cs

--- ticket23.cs
+++ ticket23.cs
@@ -4,10 +4,14 @@ using System;
 //билет 23
 namespace Ticket
 {
+    // создаем класс Book
     class Book
     {
+        //автореализующие свойства Author Title
         public string Author {set; get;}
         public string Title {set; get;}
+
+        //создаем конструктор класса 
         public Book(string author,string title)
         {
             Author = author;
@@ -20,6 +24,7 @@ namespace Ticket
         
         static void Main(string[] args)
         { 
+            //создаем экземпляр   и передаем туда значения
             Book book = new Book("Dalina",",билеты филимонова");
         }
     }
